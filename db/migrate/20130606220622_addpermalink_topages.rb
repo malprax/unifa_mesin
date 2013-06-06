e4ddbe2d592b15db38e188424ac59e0508e50bc8
aulia <kingmalprax@gmail.com>
2013-06-07 07:21:02 +0800

    sudahtambahpermalinkdanhapusdbmigratesalah

--- db/migrate/20130606220622_addpermalink_topages.rb
+++
@@ -1,7 +0,0 @@
-class AddpermalinkTopages < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
--- db/migrate/20130606220855_addpermalink_topage.rb
+++
@@ -1,7 +0,0 @@
-class AddpermalinkTopage < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
