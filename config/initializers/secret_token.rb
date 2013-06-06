c4b06da1971978b09e5ea2ee9344b30b9a7f9889
aulia <kingmalprax@gmail.com>
2013-06-06 11:42:54 +0800

    baru buat controller pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unifa::Application.config.secret_token = '138d80419502df9ca3fdf2a0468c3f8cbf0f67a198bcb67159912677001659e449c3a4271b6dec87113191a9f96786088dc7a3af7bf517177bdd577969e55bd7'
