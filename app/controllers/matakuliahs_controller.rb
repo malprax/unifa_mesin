54fe761c1791230dff69941f4d7154b2bed241c8
aulia <kingmalprax@gmail.com>
2013-06-17 13:52:04 +0800

    tambah filter authentifocation untuk matakuliah controller

--- app/controllers/matakuliahs_controller.rb
+++ app/controllers/matakuliahs_controller.rb
@@ -1,4 +1,5 @@
 class MatakuliahsController < ApplicationController
+  before_filter :authenticate_user!
   def index
     @matakuliah = Matakuliah.all
   end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  before_filter :authenticate_user!, except: [:index, :show]
+  before_filter :authenticate_user!, except: [:show]
   def index
     @pages = Page.all
   end
