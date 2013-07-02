c4d37346ca2e711c7fba3537dd7a0ce882ab288a
aulia <kingmalprax@gmail.com>
2013-07-02 15:20:53 +0800

    hilangkan halaman daftar2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable and :registerable
-  devise :database_authenticatable,:recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable,:recoverable, :rememberable, :trackable, :validatable, :registerable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :role_id,
