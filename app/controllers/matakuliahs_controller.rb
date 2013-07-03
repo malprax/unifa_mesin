6aa492e7bd6150f0e424538144d281f4d8b39b53
aulia <kingmalprax@gmail.com>
2013-07-03 14:26:16 +0800

    fix sorting2

--- app/controllers/matakuliahs_controller.rb
+++ app/controllers/matakuliahs_controller.rb
@@ -1,8 +1,11 @@
 class MatakuliahsController < ApplicationController
   load_and_authorize_resource
   before_filter :authenticate_user!, except: [:index]
+  helper_method :sort_column, :sort_direction
+  
+  
   def index
-    @matakuliahs = Matakuliah.order(params[:sort])
+    @matakuliahs = Matakuliah.order(sort_column+"  "+sort_direction)
   end
   def new
     @matakuliah = Matakuliah.new
@@ -16,6 +19,7 @@ class MatakuliahsController < ApplicationController
       render "new"
     end
   end
+  
   
   def edit
     @matakuliah = Matakuliah.find(params[:id])
@@ -36,5 +40,19 @@ class MatakuliahsController < ApplicationController
     @matakuliah.destroy
     redirect_to matakuliahs_path, :notice => 'Mata Kuliah Berhasil Dihapus.'
   end
+  
+  private
+  def sort_column
+    Matakuliah.column_names.include?(params[:sort]) ? params[:sort] : "kode"   
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ?  params[:direction] : "asc"  
+  end
+  
+    
+  
+  
+  
   
 end
