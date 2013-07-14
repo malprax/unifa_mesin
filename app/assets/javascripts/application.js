b10498d0b1179595af509b6828c63ac36dc71989
aulia <kingmalprax@gmail.com>
2013-07-14 15:08:08 +0800

    fix step 1 datatables

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,11 +11,8 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
-
-
 //= require jquery_ujs
-
-
+//= require dataTables/jquery.dataTables
 //= require twitter/bootstrap
 
 //= require ckeditor-jquery
--- app/controllers/matakuliahs_controller.rb
+++ app/controllers/matakuliahs_controller.rb
@@ -3,18 +3,31 @@ class MatakuliahsController < ApplicationController
   before_filter :authenticate_user!, except: [:index]
   def index
     @matakuliah = Matakuliah.all
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @matakuliah }
+    end
   end
   def new
     @matakuliah = Matakuliah.new
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @matakuliah }
+    end
   end
   
   def create
     @matakuliah = Matakuliah.new(params[:matakuliah])
-    if @matakuliah.save
-      redirect_to matakuliahs_path, :notice => 'Mata Kuliah Berhasil Dibuat.'     
-    else
-      render "new"
+    respond_to do |format|
+      if @matakuliah.save
+        format.html { redirect_to @matakuliah, :notice => 'Mata Kuliah Berhasil Dibuat.' }
+        format.json { render json: @matakuliah, status: :created, location: @matakuliah }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @matakuliah.errors, status: :unprocessable_entity }
+      end
     end
+    
   end
   
   def edit
@@ -24,17 +37,25 @@ class MatakuliahsController < ApplicationController
   
   def update
     @matakuliah = Matakuliah.find(params[:id])
-    if @matakuliah.update_attributes(params[:matakuliah])
-      redirect_to matakuliahs_path, :notice => 'Mata Kuliah Berhasil Diperbaharui.'
-    else
-      render 'edit'
+        
+    respond_to do |format|
+      if @matakuliah.update_attributes(params[:matakuliah])
+        format.html { redirect_to @matakuliah, :notice => 'Mata Kuliah Berhasil Diperbaharui.'}
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @information.errors, status: :unprocessable_entity }
+      end
     end
   end
   
   def destroy
     @matakuliah = Matakuliah.find(params[:id])
-    @matakuliah.destroy
-    redirect_to matakuliahs_path, :notice => 'Mata Kuliah Berhasil Dihapus.'
+    @matakuliah.destroy    
+    respond_to do |format|
+      format.html { redirect_to matakuliahs_path, :notice => 'Mata Kuliah Berhasil Dihapus.' }
+      format.json { head :no_content }
+    end
   end
   
 end
